7b753cc04e0713a2da4f5d9ed0fa1ba75e12322e
Felipe Nogueira de Lima <164098679+felipelima177@users.noreply.github.com>
2024-06-04 23:58:14 -0300

    Update - adição da função alert para cada arma

---
+++ TCC_SITES/ALPHA_STRIKE/PAGES/loja.js
@@ -0,0 +1,26 @@
+// * FUNÇÃO PARA ADICIONAR AS ARMAS AO CARRINHO *//
+function comprarM40() {
+  alert(
+    "Sniper de Airsoft M40 SA-S02 Core S-Series Tan adicionada ao carrinho!"
+  );
+}
+
+function comprarCM356() {
+  alert("CYMA CM.356 Tactical adicionada ao carrinho!");
+}
+
+function comprarGLOCK() {
+  alert("Umarex GLOCK 17 Gen3 CO2 adicionada ao carrinho!");
+}
+
+function comprarCM373() {
+  alert("CYMA CM.373M M1014 adicionada ao carrinho!");
+}
+
+function comprarUSP() {
+  alert("Umarex H&K USP .45 GBB adicionada ao carrinho!");
+}
+
+function comprarSSG10() {
+  alert("Novritsch SSG10 A1 adicionada ao carrinho!");
+}
